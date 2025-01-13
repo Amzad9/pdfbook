91d353b509f0bfd68d18c6b9f5b418eea8a852cf
amzad949 <amzad.khan@ftachdata.com>
2025-01-13 17:56:21 +0530

    add loader

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { lazy, Suspense } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Loader from './components/Loader'
 const Home = lazy(() => import('./pages/Home'))
 const About = lazy(() => import('./pages/About'))
 const Foreword = lazy(() => import('./pages/Foreword'))
@@ -10,23 +11,45 @@ const MessageSindh = lazy(() => import('./pages/MessageSindh'))
 const Introduction = lazy(()=> import('./pages/Introduction'))
 const WaterResources = lazy(() => import('./pages/WaterResources'))
 const ClimateDynamics = lazy(() => import('./pages/ClimateDynamics'))
+
 function App() {
 
   return (
     <>
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<Loader />}>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />}>
-            <Route path='about' element={<About />} />
-            <Route path='forward' element={<Foreword />} />
-            <Route path='message' element={<Message />} />
-            <Route path='messagesindh' element={<MessageSindh />} />
-            <Route path='introduction' element={<Introduction />} />
-            <Route path='waterresources' element={<WaterResources />} />
-            <Route path='climatedynamics' element={<ClimateDynamics />} />
-          </Route>
-        </Routes>
+        <Route path='/' element={<Home />}>
+          <Route 
+            path='about' 
+            element={<Suspense fallback={<Loader />}><About /></Suspense>} 
+          />
+          <Route 
+            path='forward' 
+            element={<Suspense fallback={<div>Loading...</div>}><Foreword /></Suspense>} 
+          />
+          <Route 
+            path='message' 
+            element={<Suspense fallback={<div>Loading...</div>}><Message /></Suspense>} 
+          />
+          <Route 
+            path='messagesindh' 
+            element={<Suspense fallback={<div>Loading...</div>}><MessageSindh /></Suspense>} 
+          />
+          <Route 
+            path='introduction' 
+            element={<Suspense fallback={<div>Loading...</div>}><Introduction /></Suspense>} 
+          />
+          <Route 
+            path='waterresources' 
+            element={<Suspense fallback={<div>Loading...</div>}><WaterResources /></Suspense>} 
+          />
+          <Route 
+            path='climatedynamics' 
+            element={<Suspense fallback={<div>Loading...</div>}><ClimateDynamics /></Suspense>} 
+          />
+        </Route>
+      </Routes> 
       </BrowserRouter>
     </Suspense>
     </>
